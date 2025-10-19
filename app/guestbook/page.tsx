237c23d5121ae67eaf1eefd797ffedaf0e2085cf
sanjaibalajee <sanjaigiri5@gmail.com>
2025-10-19 23:37:35 +0530

    force dynamic guest page

--- app/guestbook/page.tsx
+++ app/guestbook/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import { Footer } from "@/components/footer"
 import { GuestbookClient } from "@/components/guestbook-client"
 import { getGuestbookEntries } from "@/app/actions/getGuestBookEntries"
